a4b97c1fe0e220e6e0ae1e09b2cd1241b2823e9c
Mohammad Sameer <sammusmarty563@gmail.com>
2024-09-18 20:58:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,8 +60,89 @@ const exchangeRate = async () => {
   ${finalResult} ${toCurrency.value}</div>
   <div class = 'valueOfFromCurrency'>1 ${fromCurrency.value} = ${rate} ${toCurrency.value}</div>
   <div  class = 'valueOfToCurrency'>1 ${toCurrency.value} = ${exchangeRateOfToCurrencytoFromCurrency} ${fromCurrency.value}</div>`;
+
+  updateTables(rate, 1 / rate);
 };
 
 convertButton.addEventListener("click", () => {
   exchangeRate();
 });
+
+function updateTables(fromCurr, toCurr) {
+  fromCurr = Math.round(fromCurr * 1000) / 1000;
+  toCurr = Math.round(toCurr * 1000) / 1000;
+  let fromCurrCode = fromCurrency.value;
+  let toCurrCode = toCurrency.value;
+
+  let displayValues = [
+    1, 5, 10, 15, 25, 50, 100, 500, 1000, 5000, 1000, 5000, 10000,
+  ];
+
+  let fromDisplayValues = [];
+  let toDisplayValues = [];
+
+  for (i = 0; i < displayValues.length; i++) {
+    fromDisplayValues.push(`${displayValues[i] * fromCurr} ${toCurrCode}`);
+    toDisplayValues.push(`${displayValues[i] * toCurr} ${fromCurrCode}`);
+  }
+
+  let fromCurrTable = document.querySelector("#fromCurrTableID tbody");
+  let toCurrTable = document.querySelector("#toCurrTableID tbody");
+
+  {
+    let fromCurrTableHead = document.querySelectorAll(".fromCurrTableHead");
+    let fromCurrH3 = document.querySelector(".fromCurrH3");
+
+    fromCurrH3.innerText = `Convert ${fromCurrCode} to ${toCurrCode}`;
+
+    for (thead of fromCurrTableHead) {
+      thead.innerHTML = `${fromCurrCode}`;
+    }
+
+    fromCurrTable.innerHTML = "";
+
+    const length = Math.min(displayValues.length, fromDisplayValues.length);
+
+    for (let i = 0; i < length; i++) {
+      const row = document.createElement("tr");
+
+      const cellDisplayValue = document.createElement("td");
+      cellDisplayValue.textContent = `${displayValues[i]} ${fromCurrCode}`;
+      row.appendChild(cellDisplayValue);
+
+      const cellFromDisplayValue = document.createElement("td");
+      cellFromDisplayValue.textContent = fromDisplayValues[i];
+      row.appendChild(cellFromDisplayValue);
+
+      fromCurrTable.appendChild(row);
+    }
+  }
+  {
+    let toCurrTableHead = document.querySelectorAll(".toCurrTableHead");
+    let toCurrH3 = document.querySelector(".toCurrH3");
+
+    toCurrH3.innerText = `Convert ${toCurrCode} to ${fromCurrCode}`;
+
+    for (thead of toCurrTableHead) {
+      thead.innerHTML = `${toCurrCode}`;
+    }
+
+    toCurrTable.innerHTML = "";
+
+    const length = Math.min(displayValues.length, toDisplayValues.length);
+
+    for (let i = 0; i < length; i++) {
+      const row = document.createElement("tr");
+
+      const cellDisplayValue = document.createElement("td");
+      cellDisplayValue.textContent = `${displayValues[i]} ${toCurrCode}`;
+      row.appendChild(cellDisplayValue);
+
+      const cellFromDisplayValue = document.createElement("td");
+      cellFromDisplayValue.textContent = toDisplayValues[i];
+      row.appendChild(cellFromDisplayValue);
+
+      toCurrTable.appendChild(row);
+    }
+  }
+}
